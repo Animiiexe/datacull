e6d9611e928b37ebd4ba9418a91b240dd8fc1991
Animesh.MS <123883011+Animiiexe@users.noreply.github.com>
2025-01-31 10:36:04 +0000

    added validator and Csv reader

---
+++ main.js
@@ -0,0 +1,28 @@
+import { intro, outro, log } from "@clack/prompts";
+import { readCSV, writeCSV } from "./lib/io.js";
+
+import prompt from "./lib/prompt.js";
+
+async function main() {
+  // Start interactive CLI
+  intro("DusterCLI app starting");
+
+  // Get user prompt
+  const { input, output, errors } = await prompt();
+
+  // Read the file
+  const data = readCSV(input);
+
+  log.message("File has been read successfully!");
+
+  // Create clean csv
+  writeCSV(output, data.body);
+
+  log.message("File has been created!");
+
+  // TODO: Create errors.csv
+
+  outro("Thanks for using our app!");
+}
+
+main();
